b28aa44bc7f438e57bd80cf6012e17e26829d337
Geetha1303 <geetha@zelarsoft.com>
2021-10-05 19:52:25 +0530

    latest

--- variables.tf
+++ variables.tf
@@ -16,6 +16,12 @@ variable "public-subnet-2-cidr" {
   default     = "10.0.1.0/24"
 }
 
+variable "public-subnet-3-cidr" {
+  description = "public subnet 3 CIDR block"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
 variable "private-subnet-1-cidr" {
   description = "private subnet 1 CIDR block"
   type        = string
@@ -26,4 +32,10 @@ variable "private-subnet-2-cidr" {
   description = "private subnet 2 CIDR block"
   type        = string
   default     = "10.0.3.0/24"
+}
+
+variable "private-subnet-3-cidr" {
+  description = "private subnet 3 CIDR block"
+  type        = string
+  default     = "10.0.5.0/24"
 }
--- vpc.tf
+++ vpc.tf
@@ -14,14 +14,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
 
 resource "aws_vpc" "vpc" {
   cidr_block       = "${var.vpc-cidr}"
@@ -56,6 +48,18 @@ resource "aws_subnet" "public-subnet-2" {
   }
 }
 
+resource "aws_subnet" "public-subnet-3" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${var.public-subnet-3-cidr}"
+  availability_zone = "us-east-1e"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public subnet 2"
+  }
+}
+
+
 resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = aws_vpc.vpc.id
 
@@ -89,6 +93,19 @@ resource "aws_route_table_association" "public-subnet-2-route-table-association"
   route_table_id = aws_route_table.public-route-table.id
 }
 
+resource "aws_route_table_association" "public-subnet-3-route-table-association" {
+  subnet_id      = aws_subnet.public-subnet-3.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+resource "aws_nat_gateway" "Nat-gateway" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.public-subnet-1.id
+  tags      = {
+    Name    = "Nat gateway"
+  }
+}
+
+
 resource "aws_subnet" "private-subnet-1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "${var.private-subnet-1-cidr}"
@@ -110,4 +127,45 @@ resource "aws_subnet" "private-subnet-2" {
     Name = "private subnet 2"
   }
 }
+
+resource "aws_subnet" "private-subnet-3" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${var.private-subnet-3-cidr}"
+  availability_zone = "us-east-1f"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private subnet 3"
+  }
+}
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_nat_gateway.Nat-gateway.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private-subnet-1-route-table-association" {
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-2-route-table-association" {
+  subnet_id      = aws_subnet.private-subnet-2.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-3-route-table-association" {
+  subnet_id      = aws_subnet.private-subnet-3.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+
 
