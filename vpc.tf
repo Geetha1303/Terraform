0594683edf269733f18af16d9436cab862686c1b
Geetha1303 <geetha@zelarsoft.com>
2021-10-05 17:34:19 +0530

    latest

--- vpc.tf
+++ vpc.tf
@@ -8,13 +8,6 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_internet_gateway" "internet-gateway" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "Test IGW"
-  }
-}
 
 resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.vpc.id
@@ -35,6 +28,14 @@ resource "aws_subnet" "public-subnet-2" {
 
   tags = {
     Name = "public subnet 2"
+  }
+}
+
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Test IGW"
   }
 }
 
