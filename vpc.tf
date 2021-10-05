910bf58543794b8fb8c9042d46483f1c731035d8
Geetha1303 <geetha@zelarsoft.com>
2021-10-05 18:54:06 +0530

    latest

--- vpc.tf
+++ vpc.tf
@@ -68,13 +68,11 @@ resource "aws_internet_gateway" "internet-gateway" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.vpc.id
 
-  route = [
-    {
+  route {
       cidr_block = "0.0.0.0/0"
       //CRT uses this IGW to reach internet
       gateway_id = aws_internet_gateway.internet-gateway.id
     }
-  ]
 
   tags = {
     Name = "Public Route Table"
