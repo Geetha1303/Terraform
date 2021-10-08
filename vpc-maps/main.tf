8221779c5584260629e1bdc832385f3ff5564e6b
Geetha1303 <geetha@zelarsoft.com>
2021-10-08 12:18:36 +0530

    latest

---
+++ vpc-maps/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile                 = "default"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  for_each = var.public_subnet_numbers
+
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = each.key
+
+  # 2,048 IP addresses each
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
+}
+
+resource "aws_subnet" "private" {
+  for_each = var.private_subnet_numbers
+
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = each.key
+
+  # 2,048 IP addresses each
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
+}
